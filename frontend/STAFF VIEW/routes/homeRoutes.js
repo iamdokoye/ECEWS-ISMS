0e72cf65bebf99b32a7612ba964e2658b43e40d8
Luceee <owner@Lucees-MacBook.local>
2025-06-02 17:05:03 +0100

    staff view log page completed

--- frontend/STAFF VIEW/routes/homeRoutes.js
+++ frontend/STAFF VIEW/routes/homeRoutes.js
@@ -37,15 +37,3 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-
-    $("#goHome").click(function (e) {
-        e.preventDefault(); // Prevent default navigation
-        let targetURL = "homeGrid.html";
-
-        $("body").fadeOut(300, function () {
-            window.location.href = targetURL; // Navigate after fade-out
-        });
-
-    });
-});
