4d5bb54b01989e32ecfc19dcaf9ebceeb640ba8b
Luceedreams <lucy.okoro001@gmail.com>
2025-06-05 11:10:46 +0100

    Final changes to HR VIEW

--- frontend/HR VIEW/routes/hrDashRoute.js
+++ frontend/HR VIEW/routes/hrDashRoute.js
@@ -2,7 +2,46 @@ $(document).ready(function () {
 
     $("#viewBtn").click(function (e) {
         e.preventDefault(); // Prevent default navigation
-        let targetURL = "homeGrid.html";
+        let targetURL = "hrGrid.html";
+
+        $("body").fadeOut(300, function () {
+            window.location.href = targetURL; // Navigate after fade-out
+        });
+
+    });
+});
+
+$(document).ready(function () {
+
+    $("#dashboardBtn").click(function (e) {
+        e.preventDefault(); // Prevent default navigation
+        let targetURL = "hrDash.html";
+
+        $("body").fadeOut(300, function () {
+            window.location.href = targetURL; // Navigate after fade-out
+        });
+
+    });
+});
+
+$(document).ready(function () {
+
+    $(".studentsCard").click(function (e) {
+        e.preventDefault(); // Prevent default navigation
+        let targetURL = "hrstudentProfile.html";
+
+        $("body").fadeOut(300, function () {
+            window.location.href = targetURL; // Navigate after fade-out
+        });
+
+    });
+});
+
+$(document).ready(function () {
+
+    $(".backArrow").click(function (e) {
+        e.preventDefault(); // Prevent default navigation
+        let targetURL = "hrGrid.html";
 
         $("body").fadeOut(300, function () {
             window.location.href = targetURL; // Navigate after fade-out
