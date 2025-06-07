9a0f074ee946ce354432dd9997d2b3aea03dd9f0
Daniel Okoye <okoyedann@gmail.com>
2025-06-07 21:07:48 +0100

    Add database connection setup and update package dependencies

--- back-end/server.js
+++ back-end/server.js
@@ -3,19 +3,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use(cors());
 const app = express();
+
+// Middleware should be added after app is initialized
+app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
-})
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "API WORKING!!!" })
 });
 
-mongoose.connect("").then(() => {
+mongoose.connect("mongodb+srv://okoyedann:FhQSPJ6WJTIpGifj@cluster0.huyfpev.mongodb.net/").then(() => {
     app.listen("4000", () => {
         console.log("Connected and listening on port 4000")
     })
