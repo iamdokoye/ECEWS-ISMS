ac5a1691d3302068aeb045a6f8ce92c362679a75
Daniel Okoye <okoyedann@gmail.com>
2025-06-09 16:51:51 +0100

    Added STAFF VIEW

---
+++ frontend/STAFF VIEW/scripts/studentProfile.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#uploadTrigger').on('click', function () {
+      $('#selectImage').click();
+    });
+  
+    $('#selectImage').on('change', function (e) {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          // Optional preview logic goes here
+        //   console.log('Image selected:', e.target.result);
+        $('.photoBox img').attr('src', e.target.result); // Preview
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  });
