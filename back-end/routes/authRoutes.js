727c9e4ae5560adc9aac58bebfe4f4b394005055
Daniel Okoye <okoyedann@gmail.com>
2025-06-15 14:25:10 +0100

    Refactor login route to simplify user retrieval logic and improve error handling

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -2,27 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 const validateEmailDomain = require('../middleware/validateEmailDomain');
-const { getUserByEmailInternal } = require('../models/userModel');
 
-// Public
 router.post('/register', register);
 
-// First tries internal, then checks external if not found
 router.post('/login', async (req, res) => {
-  try {
-    const { email } = req.body;
-    console.log('Login request for:', email);
-    
-    const internalUser = await getUserByEmailInternal(email);
-
-    if (internalUser) {
-      return login(req, res);
-    } else {
-      validateEmailDomain(req, res, () => login(req, res));
-    }
-  } catch (err) {
-    console.error('Error in /login route:', err);
-    res.status(500).json({ message: 'Internal error' });
+  const { email } = req.body;
+  const internalUser = await require('../models/userModel').getUserByEmailInternal(email);
+  if (internalUser) {
+    return require('../controllers/authController').login(req, res);
+  } else {
+    // No timeout needed anymore
+    validateEmailDomain(req, res, () => require('../controllers/authController').login(req, res));
   }
 });
 
