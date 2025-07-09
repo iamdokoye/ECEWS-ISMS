70edbaa06f13069dc8371e4cba28cf62bebf6f2b
Daniel Okoye <okoyedann@gmail.com>
2025-07-09 12:07:01 +0100

    Refactor assignSupervisorRoutes to use internal functions for fetching units and users

--- api/routes/assignSupervisorRoutes.js
+++ api/routes/assignSupervisorRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getUnitsFromExternal,
-  getUsersByUnitExternal,
+  getUnits,
+  getUsersByUnit,
   assignSupervisor
 } = require('../controllers/assignSupervisorController');
 
